de481d7359e3a0d6dbc5abb04eb66314aeff56bb
RahimovIR <rahimovir@gmail.com>
2014-08-28 23:51:51 +0600

    Fix setup.py for python 3.4

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.md"),
+    long_description=str(read("README.rst")),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="MIT",
