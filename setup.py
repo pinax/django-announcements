cc5b743f042c24c64df87d1525984e36fe9e9476
KatherineMichel <kthrnmichel@gmail.com>
2018-01-18 15:22:27 -0600

    Fixing setup.py format

--- setup.py
+++ setup.py
@@ -11,8 +11,8 @@ Pinax Announcements
 
 .. image:: https://img.shields.io/pypi/v/pinax-announcements.svg
     :target: https://pypi.python.org/pypi/pinax-announcements/
-.. image:: https://img.shields.io/badge/license-MIT-blue.svg
-    :target: https://pypi.python.org/pypi/pinax-announcements/
+
+\ 
 
 .. image:: https://img.shields.io/circleci/project/github/pinax/pinax-announcements.svg
     :target: https://circleci.com/gh/pinax/pinax-announcements
@@ -25,8 +25,14 @@ Pinax Announcements
 .. image:: https://img.shields.io/github/issues-pr-closed/pinax/pinax-announcements.svg
     :target: https://github.com/pinax/pinax-announcements/pulls?q=is%3Apr+is%3Aclosed
 
+\ 
+
 .. image:: http://slack.pinaxproject.com/badge.svg
     :target: http://slack.pinaxproject.com/
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg
+    :target: https://opensource.org/licenses/MIT/
+
+\ 
 
 ``pinax-announcements`` is a well tested, documented, and proven solution
 for any site wanting announcements for it's users.
@@ -48,11 +54,11 @@ Supported Django and Python Versions
 ------------------------------------
 
 +-----------------+-----+-----+-----+-----+
-| Django \ Python | 2.7 | 3.4 | 3.5 | 3.6 |
+| Django / Python | 2.7 | 3.4 | 3.5 | 3.6 |
 +=================+=====+=====+=====+=====+
-| 1.11            |  *  |  *  |  *  |  *  |
+|  1.11           |  *  |  *  |  *  |  *  |
 +-----------------+-----+-----+-----+-----+
-| 2.0             |     |  *  |  *  |  *  |
+|  2.0            |     |  *  |  *  |  *  |
 +-----------------+-----+-----+-----+-----+
 """
 
@@ -73,18 +79,23 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Environment :: Web Environment",
         "Framework :: Django",
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 1.8",
+        "Framework :: Django :: 2.0",
         "Programming Language :: Python",
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
