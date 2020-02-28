b26420cbf72269f58436f390abbe88bb89194b61
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-02-29 00:15:57 +0100

    Update support documentation in setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ Pinax Announcements
 .. image:: https://img.shields.io/pypi/v/pinax-announcements.svg
     :target: https://pypi.python.org/pypi/pinax-announcements/
 
-\ 
+\
 
 .. image:: https://img.shields.io/circleci/project/github/pinax/pinax-announcements.svg
     :target: https://circleci.com/gh/pinax/pinax-announcements
@@ -25,14 +25,14 @@ Pinax Announcements
 .. image:: https://img.shields.io/github/issues-pr-closed/pinax/pinax-announcements.svg
     :target: https://github.com/pinax/pinax-announcements/pulls?q=is%3Apr+is%3Aclosed
 
-\ 
+\
 
 .. image:: http://slack.pinaxproject.com/badge.svg
     :target: http://slack.pinaxproject.com/
 .. image:: https://img.shields.io/badge/license-MIT-blue.svg
     :target: https://opensource.org/licenses/MIT/
 
-\ 
+\
 
 ``pinax-announcements`` is a well tested, documented, and proven solution
 for any site wanting announcements for it's users.
@@ -53,13 +53,15 @@ Announcements have title and content, with options for filtering their display:
 Supported Django and Python Versions
 ------------------------------------
 
-+-----------------+-----+-----+-----+-----+
-| Django / Python | 2.7 | 3.4 | 3.5 | 3.6 |
-+=================+=====+=====+=====+=====+
-|  1.11           |  *  |  *  |  *  |  *  |
-+-----------------+-----+-----+-----+-----+
-|  2.0            |     |  *  |  *  |  *  |
-+-----------------+-----+-----+-----+-----+
++-----------------+-----+-----+-----+
+| Django / Python | 3.6 | 3.7 | 3.8 |
++=================+=====+=====+=====+
+|  1.11           |  *  |  *  |     |
++-----------------+-----+-----+-----+
+|  2.2            |  *  |  *  |  *  |
++-----------------+-----+-----+-----+
+|  3.0            |  *  |  *  |  *  |
++-----------------+-----+-----+-----+
 """
 
 setup(
@@ -80,17 +82,16 @@ setup(
         "Environment :: Web Environment",
         "Framework :: Django",
         "Framework :: Django :: 1.11",
-        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.0",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     install_requires=[
