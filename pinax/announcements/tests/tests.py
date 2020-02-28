e7434c5aa5a3619ab2866b4585d58ee6449028cb
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-02-28 23:33:01 +0100

    Drop compatability handling module

--- pinax/announcements/tests/tests.py
+++ pinax/announcements/tests/tests.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from django.conf import settings
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.sessions.models import Session
@@ -7,7 +9,6 @@ from django.utils import timezone
 
 from test_plus.test import TestCase
 
-from ..compat import mock
 from ..models import Announcement, Dismissal
 from ..templatetags.pinax_announcements_tags import \
     announcements as announcements_tag
