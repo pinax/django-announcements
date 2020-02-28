729b5eb4991b3cfcbf5fbd86804bbc9fb91fc66c
Mfon Eti-mfon <mfonetimfon@gmail.com>
2020-02-28 23:11:36 +0100

    Remove unsupported Python and Django versions from test matrix

--- pinax/announcements/models.py
+++ pinax/announcements/models.py
@@ -2,11 +2,9 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
-@python_2_unicode_compatible
 class Announcement(models.Model):
     """
     A single announcement.
