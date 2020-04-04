04e04ced6b7fb070e6fed4dec49c5fe3bd2747ed
Rick Westera <rick@ominatlas.com>
2020-04-05 09:42:00 +1200

    Update models.py
    chore: Update for Django 3

--- pinax/announcements/models.py
+++ pinax/announcements/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
+from six import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
